125a377ed0737d62820fc24376d74894cef31509
Richard <krsw460@gmail.com>
2022-02-07 02:41:56 -0500

    functioning

--- script.js
+++ script.js
@@ -2,15 +2,21 @@
 
 const container = document.querySelector("#container");
 container.style.backgroundColor = "white";
+let inputSize = 16;
 
 //Function to set up the grid, taking size input
 function createGrid(inputSize){
+
+    let squareSize = document.getElementById("container").clientHeight/inputSize;
+
     for (let i = 0; i < inputSize; i++){
         let div = document.createElement("div");
         
         for (let x = 0; x < inputSize; x++){
             let box = document.createElement("div");
             box.setAttribute("class","box");
+            box.style.width = squareSize +"px";
+            box.style.height = squareSize +"px";
             
             box.addEventListener("mouseover", (e) =>{
                 e.target.style.backgroundColor = "black";
@@ -26,8 +32,13 @@ function createGrid(inputSize){
 
 function resetColor(){
     while (container.firstChild) container.removeChild(container.firstChild);
-    createGrid(16);
+    createGrid(inputSize);
 
+}
+
+function newGrid(input=16){
+    inputSize = input;
+    resetColor();
 }
 
 //Creating the default grid with 16x16 dimensions
